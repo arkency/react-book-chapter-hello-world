1e375478df11b901408bbb69de141239e38093a4
Piotr Macuk <piotr@macuk.pl>
2015-08-18 15:12:08 +0200

    Handle click event by dumping it to the console

--- src/hello.js
+++ src/hello.js
@@ -1,9 +1,13 @@
 class HelloWorld extends React.Component {
+  linkClicked(event) {
+    console.log(event);
+  }
+
   render() {
     return (
       <div id="two-tags">
         <h1>Hello {this.props.name}!</h1>
-        <a href="#" id="click">Click me</a>
+        <a href="#" id="click" onClick={this.linkClicked}>Click me</a>
       </div>
     );
   }
