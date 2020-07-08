a4b12a121e8a65d2b7c6dba6b87e83a55403e5a8
Tomasz Patrzek <patrzek.tomasz@gmail.com>
2020-07-08 11:16:14 +0200

    Use functional components

--- src/index.js
+++ src/index.js
@@ -1,49 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-class OneTimeClickLink extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { clicked: false };
-    this.linkClicked = this.linkClicked.bind(this);
+function OneTimeClickLink(props) {
+  const [clicked, setClicked] = useState(false);
+
+  function linkClicked(event) {
+    setClicked(true);
   }
 
-  linkClicked(event) {
-    this.setState({ clicked: true });
-  }
-
-  render() {
-    if (this.state.clicked === false) {
-      return (
-        <a href="#" id="click" onClick={this.linkClicked}>
-          Click me
-        </a>
-      );
-    } else {
-      return <span>You clicked the link</span>;
-    }
-  }
+  return clicked ? (
+    <span>You clicked the link</span>
+  ) : (
+    <a href="#" id="click" onClick={linkClicked}>
+      Click me
+    </a>
+  );
 }
 
-class HelloWorld extends React.Component {
-  render() {
-    return (
-      <h1>
-        Hello {this.props.name}!
-      </h1>
-    );
-  }
+function HelloWorld(props) {
+  return <h1>Hello {props.name}!</h1>;
 }
 
-class HelloWorldApp extends React.Component {
-  render() {
-    return (
-      <div id="two-tags">
-        <HelloWorld name="Peter" />
-        <OneTimeClickLink />
-      </div>
-    );
-  }
+function HelloWorldApp(props) {
+  return (
+    <div id="two-tags">
+      <HelloWorld name="Peter" />
+      <OneTimeClickLink />
+    </div>
+  );
 }
 
 ReactDOM.render(<HelloWorldApp />, document.getElementById("root"));
